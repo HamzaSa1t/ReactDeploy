60c45669480b3665b2b86d21c1e8d3359660349e
HamzaSait <37mezo@gmail.com>
2025-05-08 01:39:29 +0300

    < ++++------_ >

--- backend/amazoo/settings.py
+++ backend/amazoo/settings.py
@@ -122,14 +122,14 @@ WSGI_APPLICATION = 'amazoo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+} """
 
-"""
+
 
 DATABASES = {
     'default': {
@@ -142,7 +142,7 @@ DATABASES = {
     }
 }
 
-"""
+
 
 # This will ensure a separate test database is used
 #['default']['NAME'] = 'test_mydb'  # Django automatically creates a test version of your database.
@@ -190,8 +190,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWS_CREDENTIALS = True
+
 """
 WE ADDED THOSE FUNCTIONS AND MADE THEM = TRUE.
 """
@@ -200,7 +199,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5175"
 ]
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWS_CREDENTIALS = True
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Replace with your SMTP server
 EMAIL_PORT = 587
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+/* import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 import Cookies from 'js-cookie';
 
@@ -30,6 +30,33 @@ api.interceptors.request.use(
     (error) => {
         return Promise.reject(error);
     }
+);
+
+export default api; */
+
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
+import Cookies from 'js-cookie';
+
+const api = axios.create({
+  baseURL: "http://localhost:8000",
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  }
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (!config.url.includes("/register") && !config.url.includes("user/signin/") && token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default api;
